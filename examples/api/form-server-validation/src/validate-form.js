b4938c57539a216c53c0bf0d4b104a5832a47fda
Matt Carroll <matt.carroll139@gmail.com>
2022-02-23 14:58:07 +1100

    Update validate-form.js

--- examples/api/form-server-validation/src/validate-form.js
+++ examples/api/form-server-validation/src/validate-form.js
@@ -1,35 +1,32 @@
-import { z } from 'zod'
+const { z } = require('zod')
 
 const bodySchema = z.object({
   submission: z.object({
     name: z.string(),
     age: z.number().optional(),
-    email: z.string().email()
-  })
+    email: z.string().email(),
+  }),
 })
 
-function post(req, res) {
+console.log('z', z)
 
+function post(req, res) {
   // Custom submission validation. You can use any validation library (or not) that you like to validate your data.
   try {
     const { submission } = bodySchema.parse(req.body)
 
     // Validation passed, return 200 to proceed with form submission
     return res.setStatusCode(200)
-  }
-  if (error) {
+  } catch (error) {
     // A user friendly error message can be shown to the user in One Blink Forms
     // by returning a 400 Status code and a JSON payload with a `message` property.
     // There is no character limit, however it is suggested to keep the message
     // short and clear.
-    return res
-      .setStatusCode(400)
-      .setPayload({
-        message: 'This is my custom friendly error message that will be shown to the user on failed validation'
-      })
+    return res.setStatusCode(400).setPayload({
+      message:
+        'This is my custom friendly error message that will be shown to the user on failed validation',
+    })
   }
-
-
 }
 
-export { post }
+module.exports = { post }
