ec7c440ceb6645685d3ac2831d6a5b2b569a20f0
Matt Carroll <matt.carroll139@gmail.com>
2026-01-28 10:21:18 +1100

    AP-6846 # Fixed json import in API hosting handler

--- rollup.api.config.js
+++ rollup.api.config.js
@@ -1,21 +1,13 @@
-import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-
 /**
  * @type {import('rollup').RollupOptions}
  */
 const config = {
-  external: 'url,process,path,module',
+  external: ['url', 'process', 'path'],
   input: './dist/api/scripts/api-handler.js',
   output: {
+    importAttributesKey: 'with',
     file: './dist/api-handler.js',
     format: 'es',
   },
-  plugins: [
-    commonjs(),
-    nodeResolve({
-      preferBuiltins: true,
-    }),
-  ],
 }
 export default config
